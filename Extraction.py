96673dc6f968959948dee2f40d242e24eb647d92
honeal3 <honeal3@gatech.edu>
2023-06-08 11:49:54 -0400

    Full market data extraction from Hypixel API

--- Extraction.py
+++ Extraction.py
@@ -11,9 +11,12 @@ data = response.json()
 # Extract data from the JSON response
 products = data['products']
 
+# Get the last updated timestamp
+last_updated = data['lastUpdated']
+
 # Prepare CSV file
 csv_file = 'market.csv'
-fieldnames = ['product_id', 'buy_price', 'sell_price', 'last_updated']
+fieldnames = ['product_id', 'buy_price', 'sell_price', 'last_updated', 'sell_volume', 'buy_volume', 'sell_orders', 'buy_orders']
 with open(csv_file, 'w', newline='') as file:
     writer = csv.DictWriter(file, fieldnames=fieldnames)
 
@@ -24,17 +27,25 @@ with open(csv_file, 'w', newline='') as file:
     for product_id, product_data in products.items():
         buy_summary = product_data.get('buy_summary')
         sell_summary = product_data.get('sell_summary')
-        last_updated = product_data['quick_status'].get('lastUpdated')
+        quick_status = product_data.get('quick_status')
 
-        if buy_summary and sell_summary:
+        if buy_summary and sell_summary and quick_status:
             buy_price = buy_summary[0]['pricePerUnit']
             sell_price = sell_summary[0]['pricePerUnit']
+            sell_volume = quick_status.get('sellVolume')
+            buy_volume = quick_status.get('buyVolume')
+            sell_orders = quick_status.get('sellOrders')
+            buy_orders = quick_status.get('buyOrders')
 
             writer.writerow({
                 'product_id': product_id,
                 'buy_price': buy_price,
                 'sell_price': sell_price,
-                'last_updated': last_updated
+                'last_updated': last_updated,
+                'sell_volume': sell_volume,
+                'buy_volume': buy_volume,
+                'sell_orders': sell_orders,
+                'buy_orders': buy_orders
             })
 
 print(f"Data has been successfully saved to '{csv_file}'.")
